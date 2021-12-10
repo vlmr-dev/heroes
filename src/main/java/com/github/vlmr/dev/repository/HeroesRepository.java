6c2139747eaad6ebe1bc2e58ee30f6503fbf3e05
vlmr-dev <vlmr.info@gmail.com>
2021-12-10 20:55:49 -0300

    Commit inicial

---
+++ src/main/java/com/github/vlmr/dev/repository/HeroesRepository.java
@@ -0,0 +1,9 @@
+package com.github.vlmr.dev.repository;
+
+import com.github.vlmr.dev.document.Heroes;
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+import org.springframework.data.repository.CrudRepository;
+
+@EnableScan
+public interface HeroesRepository extends CrudRepository<Heroes, String>{
+}
